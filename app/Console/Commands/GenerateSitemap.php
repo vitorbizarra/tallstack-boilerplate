dce99b05561248555ed1473c74cb40d813ebaea0
vitorbizarra <vitorbizarracontato@gmail.com>
2024-04-25 20:53:59 -0300

    :heavy_plus_sign: build: spatie/laravel-sitemap

---
+++ app/Console/Commands/GenerateSitemap.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Spatie\Sitemap\SitemapGenerator;
+
+class GenerateSitemap extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:generate-sitemap';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generate Sitemap for the site';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        SitemapGenerator::create(config('app.url'))->getSitemap()->writeToDisk('public', 'sitemap.xml');
+    }
+}
